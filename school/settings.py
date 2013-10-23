6726c3b9fbb606fd4fb020342ec14155ef2634da
pulkitpahwa <pulkitpahwa11@gmail.com>
2013-10-24 00:52:28 +0530

    Notice section added to admin

--- school/settings.py
+++ school/settings.py
@@ -99,7 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    'management',
+    'coke',
 )
 
 LOGGING = {
--- school/views.py
+++ school/views.py
@@ -3,69 +3,79 @@ from django.template import Context,Template
 from django.template.loader import get_template
 from django.shortcuts import render
 from django.views.generic import CreateView
-from management.models import Holidays,Prospective_student,current_student, Event,Exam_rule
+from coke.models import Notice
 
 def gallery(request) :
 	a = get_template("gallery.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 def home(request) :
 	a = get_template("index.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 
 def gallery(request) :
 	a = get_template("gallery.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 def alumni(request) :
 	a = get_template("alumni.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 
 def about(request) :
 	a = get_template("about.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 def parent(request) :
 	a = get_template("parent.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 def academics(request) :
 	a = get_template("academics.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 def achievements(request) :
 	a = get_template("alumni.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 
 def activities(request) :
 	a = get_template("activities.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
 
 def facilities(request) :
 	a = get_template("facilities.html")
-	c =Context({'title':"sitemap"})
+	w = Notice.objects.all()
+	c =Context({'title':"sitemap",'w':w})
 	html = a.render(c)
 	return HttpResponse(html)
 
